15bff26d1a6f1c43ba9435d006a0d14b3ae42d56
Cansur <tjswo0682@gmail.com>
2024-12-29 19:05:56 +0900

    first commit

---
+++ src/main/java/com/together/noteback/NotebackApplication.java
@@ -0,0 +1,13 @@
+package com.together.noteback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotebackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotebackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/together/noteback/NotebackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.together.noteback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotebackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
